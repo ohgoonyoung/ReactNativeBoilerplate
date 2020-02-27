5609fc62b96fc57993c80f75c9c091b9474521f4
ohgoonyoung <dhrnsdud344@gmail.com>
2020-02-28 00:58:21 +0900

    개발 환경 셋팅중

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+interface Props {}
+
+const App = ({}: Props) => {
+  return (
+    <View>
+      <Text>Text</Text>
+    </View>
+  );
+};
+
+export default App;
